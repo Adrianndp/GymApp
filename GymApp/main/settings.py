c81124ec670867fae2bd49a96dcee0255c546714
Adrian <adrianndp@hotmail.com>
2022-09-13 21:21:29 +0200

    secret key protected

--- GymApp/main/settings.py
+++ GymApp/main/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d@*v!t4ls3!5kq(vw(3h=rk3i*=qm&+0bu673)poe#23=uscc!'
+SECRET_KEY = env.str('SECRET_KEY', default='defaul')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

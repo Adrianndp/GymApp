1f1432a70c994f94c25621513ae7cfddf4805a39
Adrian <adrianndp@hotmail.com>
2022-09-15 13:22:54 +0200

    routes added

---
+++ GymApp/frontend/src/components/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from './Home';
+import { Test } from './Test';
+
+export const HomePage = () => {
+  return (
+    <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/test" element={<Test />} /> 
+        </Routes>
+    </Router>
+  );
+}
+

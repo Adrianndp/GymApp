2030e0f238a2515dae4f101fad09cc3845fa2792
Adrian <adrianndp@hotmail.com>
2022-09-15 22:32:55 +0200

    hero added

--- GymApp/frontend/src/components/Home.js
+++ GymApp/frontend/src/components/Home.js
@@ -1,8 +1,11 @@
 // Welcome Page
 import React from 'react'
+import { Hero } from "./Hero";
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div>    
+      <Hero />
+    </div>
   )
 }

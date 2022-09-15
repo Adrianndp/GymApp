c906159604fb440130a6552d57c5163b2ae867b8
Adrian <adrianndp@hotmail.com>
2022-09-15 21:33:14 +0200

    navbar inserted

--- GymApp/frontend/src/components/App.js
+++ GymApp/frontend/src/components/App.js
@@ -13,7 +13,7 @@ export default class App extends Component {
 
     render() {
         return (
-            <Box width="400px">
+            <Box width="100%">
                 <Navbar />
                 <HomePage />
                 <Footer />

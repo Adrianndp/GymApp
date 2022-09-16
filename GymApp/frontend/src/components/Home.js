e4dabd94bed9847619533faeb118b6f6f1a43cda
Adrian <adrianndp@hotmail.com>
2022-09-16 14:27:44 +0200

    bug fixed

--- GymApp/frontend/src/components/Home.js
+++ GymApp/frontend/src/components/Home.js
@@ -1,7 +1,6 @@
 // Welcome Page
 import React from 'react'
 import { Hero } from "./Hero";
-import { ContactPopper } from "./ContactPopper"
 import { TextBlock } from './TextBlock';
 import { Footer } from './Footer';
 

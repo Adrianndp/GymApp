7b1bfc3f95b906646dfbb2036493c2e55c4831f9
Adrian <adrianndp@hotmail.com>
2022-09-18 22:24:28 +0200

    without 3d

--- GymApp/frontend/src/components/Test.js
+++ GymApp/frontend/src/components/Test.js
@@ -1,8 +1,7 @@
-import Spline from '@splinetool/react-spline';
 import React from 'react'
 
 export default function Test() {
   return (
-    <Spline scene="https://prod.spline.design/TOYFg4IRwxpuF1rn/scene.splinecode" />
+    <div>Testing rutine</div>
   );
 }

abfaf56ac9c9ffcd901c8c0d240693938676466c
Adrian <adrianndp@hotmail.com>
2022-09-28 21:33:03 +0200

    iterated over the json list

--- GymApp/frontend/src/components/Home.js
+++ GymApp/frontend/src/components/Home.js
@@ -9,25 +9,22 @@ import { Exercises } from './Exercises';
 
 export const Home = () => {
 
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
 
   useEffect(()=>{
-      // here fetch logic only will run once "componentdidmount"
       fetch("/api/get_muscles")
         .then(response => response.json())
-        .then(json => setData(json))
+        .then(json => setData(json.data))
         .catch(err => {
-          // Do something for an error here
           console.log("Error Reading data " + err);
         });
-      console.log("rendered");
   }, [])
 
   return (
     <div>  
       <Hero />
       <Container maxWidth="sm">
-        {JSON.stringify(data)}
+        {data.map(d => (<li>{d}</li>))} 
         <TextwithImage title="Design your own rutine!" image_file_name="workout.jpg"/> 
         <Box textAlign='center'>
           <Button href="test" className='button'>Try it out</Button>

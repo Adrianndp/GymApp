0d63ccee422509e22e680afe06a71acb5e3875ea
Adrian <adrianndp@hotmail.com>
2022-09-29 19:53:43 +0200

    exercise viw added

--- GymApp/frontend/src/components/Exercises.js
+++ GymApp/frontend/src/components/Exercises.js
@@ -6,22 +6,14 @@ import Exercise from './Exercise';
 export const Exercises = (props) => {
   return (
     <div>
-    <h1>Exercises by Muscle Groups</h1>
-        {props.muscles.map(d => (<li>{d}</li>))} 
+        <h1>Exercises by Muscle Groups</h1>
         <Typography pt={10} pb={10}>
-            <Grid container spacing={4}>
-                <Grid item md={3}>
-                    <Exercise name="Chest" />
-                </Grid>
-                <Grid item md={3}>
-                    <Exercise name="Back" />
-                </Grid>
-                <Grid item md={3}>
-                    <Exercise name="Legs" />
-                </Grid>
-                <Grid item md={3}>
-                    <Exercise name="Biceps" />
-                </Grid>
+            <Grid container spacing={4}  justifyContent="center" alignItems="center">
+                {props.muscles.map(d => (
+                    <Grid item xs={6} md={3}>
+                        <Exercise name={d} />
+                    </Grid>
+                ))} 
             </Grid>
         </Typography>
     </div>

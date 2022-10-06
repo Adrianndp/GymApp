9150b19717e0e7aa52a5cb56afec577c7813e987
Adrian <adrianndp@hotmail.com>
2022-10-06 21:48:20 +0200

    form added

--- GymApp/frontend/src/components/Exercise.js
+++ GymApp/frontend/src/components/Exercise.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect } from 'react'
+import { Container } from '@mui/material';
 import { useParams } from "react-router-dom";
 
 export const Exercise = () =>{
@@ -18,7 +19,9 @@ export const Exercise = () =>{
 
     return (
         <div>
-            { data }
+            <Container maxWidth="md">
+                { data }
+            </Container>
         </div>
         
     )
